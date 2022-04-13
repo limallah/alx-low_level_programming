6574b449a4b1f15a0a5e2793d6ddbd2af2e83135
limallah <sadeeyahibraheem@gmail.com>
2022-04-13 16:49:48 -0700

    main

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
+
+4. Move to task 0 to task 3 and start your usual work as you know.
+-Note this=> the files for each task is before the main.h file beside it. eg 0-main.c
+-Create the file using vi or vim
+-Then add the codes in the vi mode and exit
+-Make it executable and git add, git commit and push
+-Check Your Score Afterwards
