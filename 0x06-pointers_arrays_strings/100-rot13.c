132f1b08994c97c7a07ac1c352287ca7266cb1d3
limallah <sadeeyahibraheem@gmail.com>
2022-04-24 15:15:34 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* rot13 - encodes a string in rot13
-* @s: string to be encoded
-*
-* Return: the resulting string
-**/
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
 char *rot13(char *s)
 {
 	int i, j;
