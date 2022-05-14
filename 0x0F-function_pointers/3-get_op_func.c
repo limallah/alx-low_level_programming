7fe7bac3d571b549bcba5f372cd850de74eed1cb
limallah <sadeeyahibraheem@gmail.com>
2022-05-14 15:19:44 -0700

    get func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,14 +1,11 @@
 #include "3-calc.h"
-#include <stdlib.h>
 
 /**
- * get_op_func - Selects the correct function to perform
- *               the operation asked by the user.
- * @s: The operator passed as argument.
- *
- * Return: A pointer to the function corresponding
- *         to the operator given as a parameter.
- */
+ * get_op_func -  selects the correct function to
+ * perform the operation asked by the user
+ * @s: input string
+ * Return: pointer to a function
+*/
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -21,8 +18,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (i < 5)
+	{
+		if (!strcmp(ops[i].op, s))
+			return (ops[i].f);
 		i++;
-
-	return (ops[i].f);
+	}
+	return (NULL);
 }
